b43d20520188e043c216a39cf083ae5f95759ddb
Loïc Brunet Lamarre <1969818@etu.cegepsth.qc.ca>
2022-05-13 14:46:28 -0400

    Ajout de quelques classes.

---
+++ TP3/CodeFirstTp3/Conference.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstTp3
+{
+    public sealed class Conference
+    {
+        public int ConferenceID { get; set; }
+    }
+}
