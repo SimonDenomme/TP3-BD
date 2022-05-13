a2f4828bc4a0cd2463991690607e1da80e9587a7
Simon Dénommé <1950618@etu.cegepsth.qc.ca>
2022-05-13 19:57:13 -0400

    migration v2

---
+++ TP3/CodeFirstTp3/Migrations/20220513235214_Init.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CodeFirstTp3.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "conference",
+                columns: table => new
+                {
+                    ConferenceID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_conference", x => x.ConferenceID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "membreComite",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CodeUtilisateur = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MotDePasse = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    Role = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_membreComite", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "participant",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nom = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Prenom = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Affiliation = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateInscription = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Dette = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_participant", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "conference");
+
+            migrationBuilder.DropTable(
+                name: "membreComite");
+
+            migrationBuilder.DropTable(
+                name: "participant");
+        }
+    }
+}
