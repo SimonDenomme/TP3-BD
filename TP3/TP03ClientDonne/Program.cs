085ddb4ac1c4294baec96e98b3c2fb876cec1126
Loïc Brunet Lamarre <1969818@etu.cegepsth.qc.ca>
2022-05-15 22:05:57 -0400

    Ajout du cotntext de donnée

---
+++ TP3/TP03ClientDonne/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP03ClientDonne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tp03BDEntities tp03BDEntities= new Tp03BDEntities();
+        }
+    }
+}
