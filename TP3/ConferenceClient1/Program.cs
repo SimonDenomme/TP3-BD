367edc6c98ed5dd879b285445d858f00955cdadf
Loïc Brunet Lamarre <1969818@etu.cegepsth.qc.ca>
2022-05-16 01:11:09 -0400

    Ajout du client

---
+++ TP3/ConferenceClient1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConferenceClient1.ServiceReference1;
+
+namespace ConferenceClient1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ParticipantServiceClient psc= new ParticipantServiceClient();
+        }
+    }
+}
