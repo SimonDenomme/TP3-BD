95899b638125fd17c8ea955ff39edbda3658f574
Loïc Brunet Lamarre <1969818@etu.cegepsth.qc.ca>
2022-05-15 23:26:10 -0400

    Ajout du service

---
+++ TP3/WcfServiceConference/IParticipantService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfServiceConference
+{
+    public interface IParticipantService
+    {
+        [OperationContract]
+        int InscrireParticipants(string prenom, string nom, string email, string affiliation, DateTime DateInscription, decimal frais);
+    }
+
+    [DataContract]
+    public class ParticipantInfo
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string Nom { get; set; }
+        [DataMember]
+        public string Prenom { get; set; }
+        [DataMember]
+        public string Email { get; set; }
+        [DataMember]
+        public string Affiliation { get; set; }
+        [DataMember]
+        public DateTime DateInscription { get; set; }
+        [DataMember]
+        public decimal? Dette { get; set; }
+        [DataMember]
+        public int? ConferenceId { get; set; }
+    }
+}
---
+++ TP3/WcfServiceConference/ParticipantService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP03ClientDonne;
+
+namespace WcfServiceConference
+{
+    internal class ParticipantService : IParticipantService
+    {
+        public int InscrireParticipants(string prenom, string nom, string email, string affiliation, DateTime DateInscription, decimal frais)
+        {
+            using (var context = new Tp03BDEntities())
+            {
+                try
+                {
+                    var p = new Participant()
+                    {
+                        Prenom = prenom,
+                        Nom = nom,
+                        Email = email,
+                        Affiliation = affiliation,
+                        DateInscription = DateInscription,
+                        Dette = frais,
+                    };
+
+                    context.Participants.Add(p);
+                    context.SaveChanges();
+
+                    return context.Participants.Find(p).Id;
+                }
+                catch { return -1; }
+            }
+        }
+    }
+}
