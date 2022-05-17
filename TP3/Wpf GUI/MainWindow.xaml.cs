59eab9e8469d2cdddaa8baa782101f4c1c312239
Loïc Brunet Lamarre <1969818@etu.cegepsth.qc.ca>
2022-05-16 23:56:35 -0400

    push

--- TP3/Wpf GUI/MainWindow.xaml.cs
+++ TP3/Wpf GUI/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPF_GUI.ServiceReference1;
 
 namespace WPF_GUI
 {
@@ -25,39 +26,43 @@ namespace WPF_GUI
         private CollectionViewSource _participantViewSource;
         private ObservableCollection<Participant> _participants;
         private ObservableCollection<MembreCO> _membreCOs;
+        ParticipantServiceClient _psc;
 
         public MainWindow()
         {
             _context = new Tp03BD();
+             _psc = new ParticipantServiceClient();
             InitializeComponent();
         }
 
         private void btnParticipant_Click(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                using (var context = _context)
-                {
-                    int confId = context.Conference.Find(1).Id;
-                    var p = new Participant()
-                    {
-                        Prenom = txtPrenom.Text,
-                        Nom = txtNom.Text,
-                        Email = txtEmail.Text,
-                        Affiliation = txtAffiliation.Text,
-                        DateInscription = DateTime.Parse(dtpDate.Text),
-                        Dette = decimal.Parse(txtDette.Text),
-                        ConferenceId = confId,
-                    };
+            //try
+            //{
+            //    using (var context = _context)
+            //    {
+            //        int confId = context.Conference.Find(1).Id;
+            //        var p = new Participant()
+            //        {
+            //            Prenom = txtPrenom.Text,
+            //            Nom = txtNom.Text,
+            //            Email = txtEmail.Text,
+            //            Affiliation = txtAffiliation.Text,
+            //            DateInscription = DateTime.Parse(dtpDate.Text),
+            //            Dette = decimal.Parse(txtDette.Text),
+            //            ConferenceId = confId,
+            //        };
 
-                    context.Participant.Add(p);
-                    context.SaveChanges();
+            //        context.Participant.Add(p);
+            //        context.SaveChanges();
 
-                    IEnumerable<Participant> ps = context.Participant.ToList();
-                    _participantViewSource.View.Refresh();
-                }
-            }
-            catch(Exception ex) { }
+            //        IEnumerable<Participant> ps = context.Participant.ToList();
+            //        _participantViewSource.View.Refresh();
+            //    }
+            //}
+            //catch(Exception ex) { }
+
+            _psc.InscrireParticipants(txtPrenom.Text, txtNom.Text, txtEmail.Text, txtAffiliation.Text, DateTime.Parse(dtpDate.Text), decimal.Parse(txtDette.Text));
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
